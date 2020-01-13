808e5dcae51422d5bb87c11dbbd4f09978e1cac8
jorgeamadosoria <darksoul.uci@gmail.com>
2020-01-12 23:53:23 -0600

    Master branch created for version 1.0

---
+++ src/main/java/org/jasr/facundia/demo/FacundiaDemoApplication.java
@@ -0,0 +1,13 @@
+package org.jasr.facundia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacundiaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacundiaDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/jasr/facundia/demo/controller/DemoController.java
@@ -0,0 +1,34 @@
+package org.jasr.facundia.demo.controller;
+
+import java.util.List;
+
+import org.jasr.facundia.Facundia;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	Facundia facundia  = new Facundia();
+	
+	@RequestMapping(path = "cardinal")
+	public @ResponseBody String cardinal(String number) {
+		return facundia.cardinal(number);
+	}
+	
+	@RequestMapping(path = "plural")
+	public @ResponseBody String plural(String word) {
+		return facundia.inflectWord(word);
+	}
+	
+	@RequestMapping(path = "syllabicate")
+	public @ResponseBody List<String> syllabicate(String word) {
+		return facundia.syllabicate(word);
+	}
+	
+	@RequestMapping(path = "conjugate")
+	public @ResponseBody String[] conjugate(String infinitive) {
+		return facundia.conjugate(infinitive);
+	}
+}
